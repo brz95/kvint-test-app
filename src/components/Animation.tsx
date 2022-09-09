0d3e539c10e803537602de2267447a185a6d2f6d
brz95 <abuizidov@bk.ru>
2022-09-09 08:33:43 +0300

    kvint

---
+++ src/components/Animation.tsx
@@ -0,0 +1,15 @@
+import Lottie from "lottie-react";
+import animationData from "../lottie/lottie.json";
+type AnimationProps = {
+  setSwitchToWidget: (end: boolean) => void;
+};
+const Animation = ({ setSwitchToWidget }: AnimationProps) => {
+
+  return (
+    <div className="lottie">
+      <Lottie animationData={animationData} loop={false} onComplete={() => setSwitchToWidget(true)} />
+    </div>
+  );
+};
+
+export default Animation;
---
+++ src/components/Widget.tsx
@@ -0,0 +1,11 @@
+const Widget = () => {
+  return (
+    <div className="widget">
+      <strong>Привет</strong>
+      <br />
+      <span>чем могу помочь?</span>
+    </div>
+  )
+}
+
+export default Widget
